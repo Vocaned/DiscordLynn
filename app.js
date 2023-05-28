ec2a80e3824f38d03bbb9826dc974df93767e89c
Fam0r <fam0r@mailbox.org>
2023-05-28 12:17:55 +0300

    Uncommited code from 2017

--- app.js
+++ app.js
@@ -77,20 +77,30 @@ client.on('message', msg => {
 
     }
 
-    if (command === "humble") {
-        if (args.length == 3) {
+    if (command === "gamedeal") {
+        if (args.length === 0) {
+            msg.channel.send("'gamedeal [name]|[where]|[ends in hours]|[link]|[additional info]");
+        } else {
+            var jargs = args.join(' ');
+            jargs = jargs.split("|");
+            var argsarray = new Array();
+            for (var i = 0; i < jargs.length; i++) {
+                argsarray.push(jargs[i]);
+            }
+
             var date1 = moment();
             date1.format();
-            date1.add('h', args[1])
+            date1.add(argsarray[2], 'h')
 
+            msg.channel.send("<@&390230221183254528>");
             msg.channel.send({
                 embed: {
                     color: 1043467,
                     title: "New game deal!",
-                    url: args[2],
+                    url: argsarray[3],
                     fields: [{
                         name: "Beep boop",
-                        value: args[0] + " steam key is free on humble bundle right now. The offer ends in " + args[1] + " hours after posting this. Get your key at " + args[2]
+                        value: argsarray[0] + " game is free on " + argsarray[1] + " right now. The offer ends in " + argsarray[2] + " hours after posting this. Get your key at " + argsarray[3]
                     }],
                     timestamp: date1,
                     footer: {
@@ -99,14 +109,56 @@ client.on('message', msg => {
                     }
                 }
             });
+            if (argsarray[4] != undefined) {
+                msg.channel.send("```Additional info: " + argsarray[4] + "```");
+            }
 
             msg.delete(5);
-        } else {
-            msg.reply("Correct format: ```" + config.prefix + "humble [GAME NAME] [HOURS UNTIL END] [LINK]```");
-            msg.reply("debug " + args.length);
         }
     }
+
+    if (command === "announce") {
+        var argss = "";
+        for (var i = 0; i < args.length; i++) {
+            argss = argss + " " + args[i];
+        }
+
+        msg.channel.send({
+            embed: {
+                color: 8521729,
+                fields: [{
+                    name: "Announcement:",
+                    value: argss
+                }]
+            }
+        });
+
+        msg.delete(5);
+    }
+
+    if (command === "eval") {
+    if(msg.author.id !== config.ownerID) return;
+    try {
+      const code = args.join(" ");
+      let evaled = eval(code);
+
+      if (typeof evaled !== "string")
+        evaled = require("util").inspect(evaled);
+
+      msg.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+      msg.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+    }
+    
 
 });
 
 client.login(token);
+
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+  }
